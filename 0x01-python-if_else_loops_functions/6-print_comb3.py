f20ab2eb471835f3138316920ddcd1f7b01cd7f1
Phillip <kantundaphillip@gmail.com>
2024-03-13 19:57:14 -0700

    comb

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for p in range(i + 1, 10):
+        if i == 8 and p == 9:
+            print("{}{}".format(i, p))
+        else:
+            print("{}{},".format(i, p), end=' ')
