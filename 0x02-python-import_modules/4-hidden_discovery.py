dca5a22e570b7bddc95efa24355e17e4eb21b57f
Phillip <kantundaphillip@gmail.com>
2024-03-18 09:11:02 -0700

    discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import  hidden_4
+    phrases = dir(hidden_4)
+    for phrase in phrases:
+        if phrase[:2] != "__":
+            print(phrase)    
