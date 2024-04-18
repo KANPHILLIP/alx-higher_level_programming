f93cea96793f895d960b8c3be627a6dc6a19ff0d
Phillip <kantundaphillip@gmail.com>
2024-04-18 16:35:27 -0700

    co-ordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,9 +46,6 @@ class Square:
 
     def my_print(self):
         """Print the square with # value"""
-        for i in range(0, self.__size):
-            [print("#", end='') for j in range(self.__size)]
-            print("")
         if self.__size == 0:
             print("")
             return
