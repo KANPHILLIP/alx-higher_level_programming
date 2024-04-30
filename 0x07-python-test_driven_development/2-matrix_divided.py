c5528f17ddd3306c45c447e6c3d90e34f7ebd014
Phillip <kantundaphillip@gmail.com>
2024-04-29 17:44:02 -0700

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-""" A module that defines matrix_divided method"""
+"""Module for matrix_divided method."""
 
 
 def matrix_divided(matrix, div):
-    """ divides each element of the matrix by div
-
+    """Divides all elements of matrix by div.
     Args:
-    matrix: list of lists containing int and float
-    div: number to divide matrix by
-
+        matrix: List of lists containing int or float
+        div: number to divide matrix by
     Returns:
-    list: lists of lists representing the matrix
-
+        list: List of lists representing divided matrix.
     Raises:
-    TypeError: if matrix is not list of lists of integers or floats
-    TypeError: if matrix row are not the same size
-    TypeError: if div is not an int or float
-    ZeroDivisionError: if div is 0
+        TypeError: If matrix is not list of lists containing int or float.
+        TypeError: If sublists are not all same size.
+        TypeError: If div is not int or float.
+        ZeroDivisionError: If div is zero.
     """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if div === 0:
-        raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists)" +
+        raise TypeError("matrix must be a matrix (list of lists) " +
                         "of integers/floats")
-
-      for row in matrix:
+    for row in matrix:
         if not isinstance(row, list) or len(row) == 0:
             raise TypeError("matrix must be a matrix (list of lists) " +
                             "of integers/floats")
@@ -41,4 +35,3 @@ def matrix_divided(matrix, div):
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/2-matrix_divided.txt")
-
