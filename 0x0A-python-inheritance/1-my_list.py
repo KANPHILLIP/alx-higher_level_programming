43313c07d744bba4daeeeece769ef7fe32aef111
Phillip <kantundaphillip@gmail.com>
2024-05-08 03:25:34 -0700

    mylist

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyList module """
+
+
+class MyList(list):
+    """ Represents a class that inherites from the list module"""
+    def print_sorted(self):
+        """method for printing sorted list"""
+        print( sorted(self))
