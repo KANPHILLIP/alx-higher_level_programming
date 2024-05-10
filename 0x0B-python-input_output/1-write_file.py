2a6adb19164b3fe0ea7c385fd3716db100bf3458
Phillip <kantundaphillip@gmail.com>
2024-05-10 13:49:40 -0700

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module for writing in a file """
+
+
+def write_file(filename="", text=""):
+    """ method that returns the number of characters written """
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
+        
