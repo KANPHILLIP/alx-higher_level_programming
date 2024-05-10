37b1bc16e0c18d6b636c341ea653837dcb8330dc
Phillip <kantundaphillip@gmail.com>
2024-05-10 13:28:59 -0700

    readfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module that reads a text file """
+
+
+def read_file(filename=""):
+    """ raed_file method """
+    with open(filename, "r", encoding="UTF-8") as f:
+       print(f.read(), end='')
