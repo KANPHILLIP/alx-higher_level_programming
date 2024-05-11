2631ace7fb1e0f30d118f92aa52884188d264f58
Phillip <kantundaphillip@gmail.com>
2024-05-11 03:48:03 -0700

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,5 +7,4 @@ def from_json_string(my_str):
     """ method that return a object represented by
     a Json string
     """
-    my_stringJSON = json.dumps(my_str)
-    return my_stringJSON
+    return json.dumps(my_str)
