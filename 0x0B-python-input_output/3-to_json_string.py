c94561cf044f98e1e097f42ce02b5f1a702cf216
Phillip <kantundaphillip@gmail.com>
2024-05-11 03:39:44 -0700

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a module to convert to json file"""
+import json
+
+
+def from_json_string(my_str):
+    """ method that return a object represented by
+    a Json string
+    """
+    my_stringJSON = json.dumps(my_str)
+    print(my_stringJSON)
