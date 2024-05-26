f1693bfb54bda1d767bded73e225b298144e8e78
Phillip <kantundaphillip@gmail.com>
2024-05-26 15:59:06 -0700

    best score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- all records with a score equal to or greater than 10
+-- in the second_table of the hbtn_0c_0 data base 
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
