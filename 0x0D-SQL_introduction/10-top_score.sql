63ecfab3770dc606650dab008575a3ca327f2245
Phillip <kantundaphillip@gmail.com>
2024-05-26 15:46:45 -0700

    top score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---  lists all records of the table second_table of the database
-SELECT score, name, FROM second_table ORDER BY score DESC;
+--  lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table ORDER BY score DESC;
