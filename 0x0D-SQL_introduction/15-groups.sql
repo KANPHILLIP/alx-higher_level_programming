535b66e0d359ab9fda0261307b1f555cc1614fd0
Phillip <kantundaphillip@gmail.com>
2024-05-27 05:47:29 -0700

    groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- number of records with the same score in second_table table
-SELECT score, COUNT(1) AS number FROM second_table GROUP BY score ORDER BY DESC;
+SELECT score, COUNT(1) AS number FROM second_table GROUP BY score ORDER BY number DESC;
