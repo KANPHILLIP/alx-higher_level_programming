79a96b1253e02b6d1624692b5eb2e4b0943980c1
Phillip <kantundaphillip@gmail.com>
2024-06-18 19:08:09 -0700

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,8 @@ const args = process.argv;
 
 if (args[2] === undefined) {
   console.log('No argument');
-} else if (args.length === 3){
-	console.log('Argument found');
+} else if (args.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
