6c76739f0a1882cb6f28eafdf593389b5408cc21
Phillip <kantundaphillip@gmail.com>
2024-06-19 16:19:14 -0700

    rectangle 3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w !== 'number' || w <= 0 || typeof h !== 'number' || h <= 0) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
