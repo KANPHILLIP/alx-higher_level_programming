19a5f4d9155daf293613a1f3eaf93b85309ea5a7
Phillip <kantundaphillip@gmail.com>
2024-06-18 20:08:40 -0700

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-args1 = process.argv[2];
-args2 = process.argv[3];
-function add(a, b){
-	return a + b;
+const args1 = process.argv[2];
+const args2 = process.argv[3];
+function add (a, b) {
+  return a + b;
 }
 const a = parseInt(args1);
-const b =parseInt(args2);
+const b = parseInt(args2);
 console.log(add(a, b));
