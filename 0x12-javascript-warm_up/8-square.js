daf937b323ac235e9641fbaa20efa11de2a0ad09
Phillip <kantundaphillip@gmail.com>
2024-06-18 20:16:49 -0700

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,12 @@ const args = process.argv[2];
 
 if (args === undefined || isNaN(parseInt(args))) {
   console.log('Missing size');
-}
-const size = parseInt(args);
-
-for (let i = 0; i < size; i++) {
-  let row = '';
-  for (let j = 0; j < size; j++) {
-    row += 'x';
-  }
-  console.log(row);
-}
+} else {
+	const size = parseInt(args);
+	for (let i = 0; i < size; i++) {
+		let row = '';
+		for (let j = 0; j < size; j++) {
+			row += 'x';
+		}
+		console.log(row);
+	}
