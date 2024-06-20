e6b1edaaa1d0864b97b229dec13120571ac1377e
Phillip <kantundaphillip@gmail.com>
2024-06-20 15:36:26 -0700

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2).map(arg => parseInt(arg, 10));
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-	args.sort((a, b) => (b - a));
-	const second = args[1];
-	console.log(second);
+  args.sort((a, b) => (b - a));
+  const second = args[1];
+  console.log(second);
 }
