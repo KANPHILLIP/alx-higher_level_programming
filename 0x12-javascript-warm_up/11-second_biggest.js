226ae476ac35a721cf2a6f16d44a1d002177ea94
Phillip <kantundaphillip@gmail.com>
2024-06-20 15:33:11 -0700

    max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@ const args = process.argv.slice(2).map(arg => parseInt(arg, 10));
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  const max = Math.max(...args);
-  console.log(max);
+	args.sort((a, b) => (b - a));
+	const second = args[1];
+	console.log(second);
 }
