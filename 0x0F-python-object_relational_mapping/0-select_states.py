7be8de3d2eb2c06807b55764b7c353c918a779f0
phillip <kantundandaphillip@gmail.com>
2024-06-23 07:33:43 -0700

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
+            port=3306
             )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
