6afaf1cbc5eda9028521e5b7f550cdcd57157cd5
phillip <kantundandaphillip@gmail.com>
2024-06-23 13:20:25 -0700

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
-            port=3306
+            port=3306,
+            charset="utf8"
             )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
