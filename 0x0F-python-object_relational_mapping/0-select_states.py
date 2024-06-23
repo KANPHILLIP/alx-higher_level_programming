7efce2ec93e208fa60af9843a67f6e25c6167d1f
phillip <kantundandaphillip@gmail.com>
2024-06-23 07:26:24 -0700

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+""" a script that list all the states in a database ordered by id in asc order """
 import MySQLdb
-db = MySQLdb.connect(
-        username = "root",
-        password = "",
-        database = "hbtn_0e_0_usa"
-        )
-cur = db.cursor()
-cur.execute("SELECT * FROM states  WHERE name LIKE 'N%' ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
-cur.close
-db.close
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        cur.close()
+        db.close()
