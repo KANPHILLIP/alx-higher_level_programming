86bed39ca6cd778ed55b8b542a550a930ba0e5ff
phillip <kantundandaphillip@gmail.com>
2024-06-23 11:43:50 -0700

    states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
-            port=3306
+            port=3306,
+            charset="utf8"
             )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
