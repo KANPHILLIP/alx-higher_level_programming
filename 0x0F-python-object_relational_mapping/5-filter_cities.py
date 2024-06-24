341682a56705709f6fa27980c2e51ffa006ef39f
phillip <kantundandaphillip@gmail.com>
2024-06-23 17:49:06 -0700

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that list all cities
+in a database
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM\
+            cities JOIN states ON cities.states_id = states.id \
+            WHERE states.name LIKE BINARY %(name)s \
+            ORDER BY cities ASC",{'name': sys.argv[4]})
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
