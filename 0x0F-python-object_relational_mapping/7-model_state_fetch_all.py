d27593534fd9e6f8d19abee3efb90b3d70d39bf5
phillip <kantundandaphillip@gmail.com>
2024-06-25 08:20:55 -0700

    model

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database"""
-import sys
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-if __name__ == "__main__":
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine(db_url)
-    session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
-    session = session()
-    for instance in session.quary(state).order_by(state.id).all():
-        print('{0}: {1}'.format(instance.id, instance.name))
-    session.close()
 
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
