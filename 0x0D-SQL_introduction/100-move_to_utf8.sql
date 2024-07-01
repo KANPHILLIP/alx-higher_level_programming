5c9656b2e44e0b30569d2cb603b7cc605132ee2f
phillip <kantundandaphillip@gmail.com>
2024-07-01 15:21:15 -0700

    move to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,10 @@
--- Write a script that converts hbtn_0c_0 database to UTF8.
-USE hbtn_0c_0
+-- script that converts database, table and field to UTF8.
+
+-- converts database to utf8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- converts table first_table to utf8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUNM name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- converts field name in first_table to utf8
+ALTER TABLE first_table MODIFY COLUMN  name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
